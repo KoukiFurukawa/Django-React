8ba828dc2aab6ab0c4b0d4fb2dd34e51c9d86fe9
kouki928 <furukawavo@gmail.com>
2024-07-01 00:13:45 +0900

    django-sample-pages

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
+@csrf_exempt
+def index(request):
+    if request.method == "GET":
+        return render(request, "app/index.html")
+    
+    elif request.method == "POST":
+        request_body = json.loads(request.body.decode('utf-8'))
+        JsonResponse({ "body" : request_body })
+        
+    else:
+        return HttpResponse(403)
