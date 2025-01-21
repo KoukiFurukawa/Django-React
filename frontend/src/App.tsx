6d3323257802fba37210e8a05c7f751fdf822f76
kouki928 <furukawavo@gmail.com>
2025-01-21 10:29:23 +0900

    nミリ秒ごとに処理を進めるサンプル

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState<number>(0);
+  const [isStart, setIsStart] = useState<boolean>(false);
+  const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+  
+  const DrawCount = async () => {
+    if (isStart) return;
+    setIsStart(true);
+    for (let i = 0; i < 100; i++) {
+      setCount(i);
+      await sleep(1000);
+    }
+  }
+
   return (
     <div className="App">
-      Hello World
+      <h1>Hello World</h1>
+      <h1> {count} </h1>
+      <button type='button' onClick={DrawCount}> カウントアップ </button>
     </div>
   );
 }
